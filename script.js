5568a3043be349f88daf08e02c112a8585987c1b
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-12-01 20:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,11 +11,20 @@ function convertToRoman(num) {
 
 	//your code here
 	let result = '';
-  
-	for (const item of romanNumerals) {
-	    while (num >= item.value) {
-		    result += item.numeral;
-		    num -= item.value;
+	let currentNum = num;
+	for (let i = 0; i < 7; i++) {
+		let count = Math.floor(currentNum / obj[i][1]);
+		if (count > 0) {
+		    result += obj[i][0].repeat(count);
+		    currentNum %= obj[i][1];
+		}
+	    if (i % 2 === 0 && i < 6 && currentNum >= obj[i + 2][1] - obj[i + 2][1] / 10) {
+		    result += obj[i + 2][0] + obj[i][0];
+		    currentNum -= obj[i + 2][1] - obj[i];
+		}
+	    if (i % 2 === 1 && i < 5 && currentNum >= obj[i + 1][1] - obj[i + 1][1] / 10) {
+		    result += obj[i + 1][0] + obj[i - 1][0];
+		    currentNum -= obj[i + 1][1] - obj[i - 1][1];
 		}
 	}
 	return result;
